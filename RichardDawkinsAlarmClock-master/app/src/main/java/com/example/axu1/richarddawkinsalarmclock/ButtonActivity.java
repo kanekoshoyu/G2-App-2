54c86637f45126b83b40b22f8925bd6ef7bd6e53
kanekoshoyu <kanekoshoyu@gmail.com>
2018-04-19 14:05:35 +0800

    Bluetooth Connection

--- RichardDawkinsAlarmClock-master/app/src/main/java/com/example/axu1/richarddawkinsalarmclock/ButtonActivity.java
+++ RichardDawkinsAlarmClock-master/app/src/main/java/com/example/axu1/richarddawkinsalarmclock/ButtonActivity.java
@@ -1,26 +1,166 @@
 package com.example.axu1.richarddawkinsalarmclock;
 
-import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.content.Intent;
+
 import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.Button;
+import android.widget.SeekBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import android.app.ProgressDialog;
+import android.bluetooth.BluetoothSocket;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.os.AsyncTask;
+
+import java.io.IOException;
+import java.util.UUID;
+
 
 public class ButtonActivity extends AppCompatActivity {
 
+    Button btnOn, btnOff, btnDis;
+    TextView lumn;
+    String address = null;
+    private ProgressDialog progress;
+    BluetoothAdapter myBluetooth = null;
+    BluetoothSocket btSocket = null;
+    private boolean isBtConnected = false;
+    //SPP UUID. Look for it
+    static final UUID myUUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
+
+        Intent newint = getIntent();
+        address = newint.getStringExtra(DeviceList.EXTRA_ADDRESS); //receive the address of the bluetooth device
+
+        //view of the ledControl
         setContentView(R.layout.ui_button);
+
+        //call the widgtes
+        btnOn = (Button)findViewById(R.id.button2);
+        btnOff = (Button)findViewById(R.id.button3);
+        btnDis = (Button)findViewById(R.id.button4);
+
+        new ConnectBT().execute(); //Call the class to connect
+
+        //commands to be sent to bluetooth
+        btnOn.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                turnOnLed();      //method to turn on
+            }
+        });
+
+        btnOff.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                turnOffLed();   //method to turn off
+            }
+        });
+
+        btnDis.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                Disconnect(); //close connection
+            }
+        });
+    }
+
+    private void Disconnect() {
+        if (btSocket!=null) {
+            //If the btSocket is busy
+            try {
+                btSocket.close(); //close connection
+            } catch (IOException e) {
+                msg("Error");
+            }
+        }
+        finish(); //return to the first layout
 
     }
 
-    protected void onButtonPressed(View v) {
-        String text_on_button = ((Button) findViewById(R.id.button_basic)).getText().toString();
-        if (text_on_button.equals("ON")){
-            text_on_button="OFF";
-        }else{
-            text_on_button="ON";
+    private void turnOffLed() {
+        if (btSocket!=null) {
+            try {
+                btSocket.getOutputStream().write("0".toString().getBytes());
+            } catch (IOException e) {
+                msg("Error");
+            }
         }
-        ((Button) findViewById(R.id.button_basic)).setText(text_on_button);
+    }
+
+    private void turnOnLed() {
+        if (btSocket!=null) {
+            try {
+                btSocket.getOutputStream().write("1".toString().getBytes());
+            } catch (IOException e) {
+                msg("Error");
+            }
+        }
+    }
+
+    // fast way to call Toast
+    private void msg(String s) {
+        Toast.makeText(getApplicationContext(),s,Toast.LENGTH_LONG).show();
+    }
+
+    protected void goClockLayout(View v) {
+        Intent k = new Intent(ButtonActivity.this, ClockActivity.class);
+        startActivity(k);
+    }
+
+    private class ConnectBT extends AsyncTask<Void, Void, Void>  {
+        // UI thread
+        private boolean ConnectSuccess = true; //if it's here, it's almost connected
+
+        @Override
+        protected void onPreExecute() {
+            progress = ProgressDialog.show(ButtonActivity.this, "Connecting...", "Please wait!!!");  //show a progress dialog
+        }
+
+        @Override
+        protected Void doInBackground(Void... devices) {
+            //while the progress dialog is shown, the connection is done in background
+            try {
+                if (btSocket == null || !isBtConnected) {
+                 myBluetooth = BluetoothAdapter.getDefaultAdapter();//get the mobile bluetooth device
+                 BluetoothDevice dispositivo = myBluetooth.getRemoteDevice(address);//connects to the device's address and checks if it's available
+                 btSocket = dispositivo.createInsecureRfcommSocketToServiceRecord(myUUID);//create a RFCOMM (SPP) connection
+                 BluetoothAdapter.getDefaultAdapter().cancelDiscovery();
+                 btSocket.connect();//start connection
+                }
+            } catch (IOException e){
+                ConnectSuccess = false;//if the try failed, you can check the exception here
+            }
+            return null;
+        }
+        @Override
+        protected void onPostExecute(Void result) {
+            //after the doInBackground, it checks if everything went fine
+            super.onPostExecute(result);
+
+            if (!ConnectSuccess){
+                msg("Connection Failed. Is it a SPP Bluetooth? Try again.");
+                finish();
+            }else{
+                msg("Connected.");
+                isBtConnected = true;
+            }
+            progress.dismiss();
+        }
     }
 }
--- RichardDawkinsAlarmClock-master/app/src/main/java/com/example/axu1/richarddawkinsalarmclock/MainActivity.java
+++ RichardDawkinsAlarmClock-master/app/src/main/java/com/example/axu1/richarddawkinsalarmclock/MainActivity.java
@@ -20,16 +20,22 @@ import java.util.Random;
 public class MainActivity extends AppCompatActivity {
 
 
+    String EXTRA_ADDRESS = "device_address";
+    String address=null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Intent newint = getIntent();
+        address = newint.getStringExtra(DeviceList.EXTRA_ADDRESS);
         setContentView(R.layout.ui_main);
     }
 
     protected void goBasicLayout(View v) {
-        Intent k = new Intent(MainActivity.this, ButtonActivity.class);
-        startActivity(k);
+        Intent i = new Intent(MainActivity.this, ButtonActivity.class);
+        //Change the activity.
+        i.putExtra(EXTRA_ADDRESS, address); //this will be received at ledControl (class) Activity
+        startActivity(i);
     }
 
     protected void goClockLayout(View v) {
